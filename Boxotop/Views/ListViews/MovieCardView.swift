a006dcd5535df0bec50b6c186a9b25071ece25f1
Seravea <romainpoyard@gmail.com>
2023-06-16 14:41:33 +0200

    Movie CardView for the movies list

--- Boxotop/Views/ListViews/MovieCardView.swift
+++ Boxotop/Views/ListViews/MovieCardView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct MovieCardView: View {
-
+    let movie: Movie
+    let index: Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(height: 140)
+                .foregroundColor(index % 2 == 0 ? .green : .gray)
+            
+            HStack(alignment: .top) {
+                Rectangle()
+                    .frame(width: 70, height: 100)
+                VStack {
+                    Text(movie.title)
+                        .font(.title)
+                    
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
 struct MovieCardView_Previews: PreviewProvider {
     static var previews: some View {
-        MovieCardView()
+        MovieCardView(movie: Movie.testingMovie, index: 1)
     }
 }
