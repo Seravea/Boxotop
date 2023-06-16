5b2e0ca088d1483da45e5523fe989a5ec81a92ca
Seravea <romainpoyard@gmail.com>
2023-06-16 12:56:56 +0200

    Creation of the Movie data Model, matching with the JSON key from https://app.quicktype.io JSON to Swift struct

--- Boxotop/DataModel/Movie.swift
+++ Boxotop/DataModel/Movie.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+// struct of the API Response from the JSON
+struct MoviesResponse: Codable {
+    let page: Int
+    let results: [Result]
+    let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+//struct of a Movie
+struct Result: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let id: Int
+    let title: String
+    let originalTitle, overview, posterPath: String
+    let mediaType: MediaType
+    let genreIDS: [Int]
+    let popularity: Double
+    let releaseDate: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case id, title
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case mediaType = "media_type"
+        case genreIDS = "genre_ids"
+        case popularity
+        case releaseDate = "release_date"
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+
+
+enum MediaType: String, Codable {
+    case movie = "movie"
+}
