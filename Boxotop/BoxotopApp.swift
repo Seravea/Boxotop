8bc40bc3baa9d0a2bdd6b90998242e0d0bdbfc6d
Seravea <romainpoyard@gmail.com>
2023-06-16 10:21:38 +0200

    Initial Commit

---
+++ Boxotop/BoxotopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxotopApp.swift
+//  Boxotop
+//
+//  Created by Romain Poyard on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoxotopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
