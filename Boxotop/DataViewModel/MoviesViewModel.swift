e6d1e548cae010c11c4432e5fc03c7dce39ab488
Seravea <romainpoyard@gmail.com>
2023-06-17 13:56:41 +0200

    Adding the MovieViewModel and the load data function for the API the Movie Database

--- Boxotop/DataViewModel/MoviesViewModel.swift
+++ Boxotop/DataViewModel/MoviesViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+@MainActor class MovieViewModel: ObservableObject {
+    @Published private(set) var boxOfficeMovies: [Movie] = []
+    let apiKey = "eed456115041deb5c36ed519eafea41a"
+    let headers = [
+        "accept": "application/json",
+        "Authorization": "Bearer eed456115041deb5c36ed519eafea41a"
+    ]
+    
+    
+    
+    func loadBoxOfficeMovies() async {
+
+        
+        do {
+            guard let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)&language=fr-FR&page=1") else {
+                print("Couldn't load URL")
+                return
+            }
+              
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            urlRequest.allHTTPHeaderFields = headers
+            
+            
+            let (data, networkResponse) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard (networkResponse as? HTTPURLResponse)?.statusCode == 200 else {
+                fatalError("Error when fetching data on the URLSession \(networkResponse)")
+            }
+            
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(MoviesResponse.self, from: data)
+            
+            self.boxOfficeMovies = decodedData.results
+        }catch {
+            fatalError("error when fetching data from TheMovieDatabase \(error)")
+        }
+        
+        
+    }
+    
+    
+}
