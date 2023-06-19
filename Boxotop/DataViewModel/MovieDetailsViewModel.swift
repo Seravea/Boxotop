0ae788081f77e4efe25f723d6df48b1cf248d8ee
Seravea <romainpoyard@gmail.com>
2023-06-19 11:34:14 +0200

    adding the loading function for similar movies by film, to showing this on DetailsView

--- Boxotop/DataViewModel/MovieDetailsViewModel.swift
+++ Boxotop/DataViewModel/MovieDetailsViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 
 @MainActor class MovieDetailsViewModel: ObservableObject {
     @Published var movieCasting: CastingResponse?
-    
+    @Published var similarMovies: [Movie] = []
     
     
     func loadMovieCasting(movieID: Int) async {
@@ -51,6 +51,45 @@ import Foundation
         
     }
     
+    
+    func loadSimilarMovies(movieID: Int) async {
+        
+        let headers = [
+            "accept": "application/json",
+            "Authorization": "Bearer eed456115041deb5c36ed519eafea41a"
+        ]
+        
+        let apiKey = "eed456115041deb5c36ed519eafea41a"
+        
+        do {
+            guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/similar?api_key=\(apiKey)&language=us-US&page=1") else {
+                print("Couldn't load URL")
+                return
+            }
+            
+            
+            var URLRequest = URLRequest(url: url)
+            URLRequest.httpMethod = "GET"
+            URLRequest.allHTTPHeaderFields = headers
+            
+            
+            let (data, networkResponse) = try await URLSession.shared.data(for: URLRequest)
+            
+            guard (networkResponse as? HTTPURLResponse)?.statusCode == 200 else {
+                fatalError("Error when fetching data on the URLSession \(networkResponse)")
+            }
+            
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(MoviesResponse.self, from: data)
+                    
+            self.similarMovies = decodedData.results
+            
+        }catch {
+            fatalError("error when fetching data from TheMovieDatabase \(error)")
+        }
+        
+        
+    }
     
     
 }
