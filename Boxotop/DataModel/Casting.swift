5d880a7ce07336783b2ebcb5243d95622ad26620
Seravea <romainpoyard@gmail.com>
2023-06-19 10:04:45 +0200

    create the ImageResponse for the loadImages function

--- Boxotop/DataModel/Casting.swift
+++ Boxotop/DataModel/Casting.swift
@@ -34,3 +34,25 @@ struct Cast: Codable {
     }
 }
 
+struct ImagesResponse: Codable {
+    let id: Int
+    let profiles: [PersonImageURL]
+}
+
+
+struct PersonImageURL: Codable {
+    let filePath: String?
+    
+    var imageURL: URL? {
+        if let path = filePath {
+            let baseURL = URL(string: "https://image.tmdb.org/t/p/w500")!
+            return baseURL.appending(path: path)
+        }else {
+            return nil
+        }
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case filePath = "file_path"
+    }
+}
